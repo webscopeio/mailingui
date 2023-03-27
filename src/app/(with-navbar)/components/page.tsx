cc99a059c92156811b8157c5c0763494687e57ce
Dan Polanský <polansky.dan@gmail.com>
2023-03-27 17:59:32 +0200

    fix: remove non-existent prop on ComponentsPreview

--- src/app/(with-navbar)/components/page.tsx
+++ src/app/(with-navbar)/components/page.tsx
@@ -3,7 +3,7 @@ import { ComponentsPreview } from "@components/ComponentsPreview";
 export default function Components() {
   return (
     <div className="mx-auto mt-40 mb-10 flex flex-col items-center justify-center max-xl:mt-28">
-      <ComponentsPreview isHomepage={false} />
+      <ComponentsPreview />
     </div>
   );
 }
