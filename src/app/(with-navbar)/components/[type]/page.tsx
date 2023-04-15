3367fc55f21000fa136cc1353eec26e2473c50c2
Hector Sosa <hello@hectorsosa.me>
2023-04-15 20:46:14 +0200

    Apply Formatting to HTML

--- src/app/(with-navbar)/components/[type]/page.tsx
+++ src/app/(with-navbar)/components/[type]/page.tsx
@@ -1,4 +1,5 @@
 import mjml2html from "mjml";
+import { format } from "prettier";
 import {
   ComponentExample,
   ComponentExampleProps,
@@ -49,16 +50,19 @@ const getComponent = (
 
   const transformedExamples = component.examples.flatMap(({ title, mjml }) => {
     try {
-      const { html, errors } = mjml2html(mjml, {
+      const htmlOutput = mjml2html(mjml, {
         /** `strict` validation throws if a wrong `mjml` is encountered. */
         validationLevel: "strict",
         keepComments: false,
       });
 
-      if (errors.length > 0) {
+      if (htmlOutput.errors.length > 0) {
         /** If the mjml parsing fails - for whatever reason - we filter the component out. */
         return [];
       }
+
+      /** `mjml` beautify has been deprecated, format using default `prettier` */
+      const html = format(htmlOutput.html, { parser: "html" });
 
       return [
         {
