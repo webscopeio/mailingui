947681882d44ddd30175877244567823c3b2f05d
Dan Polanský <polansky.dan@gmail.com>
2023-04-26 16:19:36 +0200

    feat: add 'Coming soon' overlay to Templates section on homepage

--- src/app/(with-navbar)/page.tsx
+++ src/app/(with-navbar)/page.tsx
@@ -20,13 +20,18 @@ const Home = () => (
     </FeaturePreview>
 
     <FeaturePreview title="Templates coming soon!" previewType="templates">
-      <div className="-mx-4 overflow-hidden">
+      <div className="relative -mx-4 overflow-hidden">
         <Image
           src={templatesPreview}
           alt="Screenshot of various templates"
           quality={100}
           className="relative left-1/2 w-full min-w-[540px] -translate-x-1/2 px-4"
         />
+        <div className="absolute inset-0 flex items-center justify-center bg-[linear-gradient(180deg,rgba(0,0,0,0.3)_0%,rgba(0,0,0,0.35)_50.1%,#000000_100%)]">
+          <div className="bg-pink-to-purple-gradient bg-clip-text text-3xl font-bold uppercase text-transparent drop-shadow-lg sm:text-6xl">
+            Coming soon
+          </div>
+        </div>
       </div>
     </FeaturePreview>
   </main>
