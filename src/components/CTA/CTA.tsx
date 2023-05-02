2f0116b50f7952028992d0b4c201638928dcb1a7
Dan Polanský <polansky.dan@gmail.com>
2023-05-02 13:13:16 +0200

    chore: remove unused 'fullWidth' prop from CTA component

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -15,20 +15,12 @@ interface AsButtonProps extends React.ComponentPropsWithoutRef<"button"> {
 type CTAProps = (AsLinkProps | AsButtonProps) & {
   /** Color of the button. */
   color: keyof typeof COLORS;
-  /** If `true`, the CTA will take up the full width of its container. */
-  fullWidth?: boolean;
 };
 
-export const CTA: React.FC<CTAProps> = ({
-  color,
-  fullWidth = false,
-  className,
-  ...delegated
-}) => {
+export const CTA: React.FC<CTAProps> = ({ color, className, ...delegated }) => {
   const mergedClassName = clsx(
     "inline-flex items-center justify-center rounded-xl py-4 px-6 font-medium hover:opacity-70",
     COLORS[color],
-    fullWidth && "w-full",
     className
   );
 
