851da1ad947754bbfd14413f87029b70a7922008
Hector Sosa <hello@hectorsosa.me>
2023-06-08 17:22:02 +0200

    Updating imports opsie

--- src/mailingui/components/texts/Text.tsx
+++ src/mailingui/components/texts/Text.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import { Text as ReactEmailText } from '@react-email/text';
+import * as React from "react";
+import { Text as ReactEmailText } from "@react-email/components";
 
 const Text: React.FC = () => {
   return <ReactEmailText>Lorem ipsum</ReactEmailText>;
 };
 
-export { Text }
+export { Text };
