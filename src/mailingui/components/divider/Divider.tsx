200124f8bd04c2e4d9927a89d64810809e84030f
Eric Valcik <48859243+ericvalcik@users.noreply.github.com>
2023-06-14 12:23:54 +0200

    Add `Divider` component and an example (#99)
    * Add divider + rename folders buttons to button and badges to badge
    
    * Working version of the Divider with text
    
    * tidy up the code
    
    * Polish the divider component
    
    * fix build
    
    * fix build #2
    
    * finish Divider component
    
    * Clean up
    
    * Clean up + in Divider add support for ThemeProvider
    
    * Make resizing of dividers responsive
    
    * Make default values more predictable
    
    * Update Divider examples
    
    * Create conditional type for DividerProps
    
    * Add missing baseWidth to Divider example
    
    * Make one example smaller and update the baseWidth accordingly

---
+++ src/mailingui/components/divider/Divider.tsx
@@ -0,0 +1,101 @@
+import React, { CSSProperties, FC, ReactNode } from "react";
+import { Hr, Section, Row, Column } from "@react-email/components";
+import { useTheme } from "../ThemeProvider/ThemeProvider";
+
+type DividerBaseProps = {
+  style?: CSSProperties;
+  borderColor?: CSSProperties["borderColor"];
+  textColor?: CSSProperties["color"];
+};
+
+type ConditionalDividerProps =
+  | {
+      children: ReactNode;
+      baseWidth?: number;
+    }
+  | { children?: never; baseWidth?: never };
+
+type DividerProps = DividerBaseProps & ConditionalDividerProps;
+
+const Divider: FC<DividerProps> = ({
+  style: styleProp,
+  borderColor,
+  textColor,
+  baseWidth = 580,
+  children,
+}) => {
+  const defaultDividerColor = "#CBD5E1";
+  const { variants } = useTheme();
+
+  const sectionBorder = {
+    width: `${Math.round(baseWidth * 0.4)}px`,
+  };
+
+  const sectionCenter: CSSProperties = {
+    width: `${Math.round(baseWidth * 0.2)}px`,
+    textAlign: "center",
+    fontSize: "14px",
+  };
+
+  if (children) {
+    return (
+      <Section
+        style={{
+          width: "100%",
+          display: "flex",
+          ...styleProp,
+        }}
+      >
+        <Row>
+          <Column style={sectionBorder}>
+            <Hr
+              style={{
+                border: "none",
+                borderTop: `1px solid ${
+                  borderColor ??
+                  variants?.secondary.borderColor ??
+                  defaultDividerColor
+                }`,
+              }}
+            />
+          </Column>
+          <Column
+            style={{
+              color: textColor ?? variants?.secondary.color ?? "#64748B",
+              ...sectionCenter,
+            }}
+          >
+            {children}
+          </Column>
+          <Column style={sectionBorder}>
+            <Hr
+              style={{
+                border: "none",
+                borderTop: `1px solid ${
+                  borderColor ??
+                  variants?.secondary.borderColor ??
+                  defaultDividerColor
+                }`,
+              }}
+            />
+          </Column>
+        </Row>
+      </Section>
+    );
+  }
+
+  return (
+    <Hr
+      style={{
+        borderTop: `1px solid ${
+          borderColor ?? variants?.secondary.borderColor ?? defaultDividerColor
+        }`,
+        margin: "20px 0",
+        width: "100%",
+        ...styleProp,
+      }}
+    />
+  );
+};
+
+export { Divider };
