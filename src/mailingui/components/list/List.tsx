6993053f5801195c138c9bc9791b77b1a46c8e38
eric valcik <eric.valcik@gmail.com>
2023-06-15 11:08:07 +0200

    Add additional props to List

--- src/mailingui/components/list/List.tsx
+++ src/mailingui/components/list/List.tsx
@@ -7,39 +7,61 @@ import React, {
   useState,
 } from "react";
 import { Column, Row } from "@react-email/components";
-import { Text, type TextProps } from "@mailingui/components";
+import { Text, type TextProps, useTheme } from "@mailingui/components";
+import { type Variant } from "@mailingui/themes";
 
 type ListContextType = {
   direction: "vertical" | "horizontal";
   centered: boolean;
   size: TextProps["size"];
+  titleSize?: TextProps["size"];
+  titleStyle?: CSSProperties;
+  bodyStyle?: CSSProperties;
+  variant: Variant;
 };
 
 const ListContext = createContext<ListContextType>({
   direction: "vertical",
   centered: false,
   size: "md",
+  titleSize: undefined,
+  variant: "default",
 });
 
-export type ListProps = {
+type SharedProps = {
   centered?: boolean;
+  titleSize?: TextProps["size"];
   size?: TextProps["size"];
   style?: CSSProperties;
-  horizontal?: boolean;
+  variant?: Variant;
+  titleStyle?: CSSProperties;
+  bodyStyle?: CSSProperties;
   children?: ReactNode;
+};
+
+export type ListProps = SharedProps & {
+  horizontal?: boolean;
 };
 
 const List: FC<ListProps> = ({
   style,
   size,
   centered,
+  titleSize,
+  variant,
   horizontal,
+  titleStyle,
+  bodyStyle,
   children,
 }) => {
   const [state, _setState] = useState<ListContextType>({
     direction: horizontal ? "horizontal" : "vertical",
     centered: !!centered,
-    size: size || "md",
+    size: size ?? "md",
+    titleSize,
+    variant: variant ?? "default",
+    titleStyle,
+    bodyStyle,
   });
 
   return (
@@ -49,23 +71,34 @@ const List: FC<ListProps> = ({
   );
 };
 
-type ListItemProps = {
-  style?: CSSProperties;
-  size?: TextProps["size"];
-  titleSize?: TextProps["size"];
-  title?: ReactNode;
-  children?: ReactNode;
+type ListItemProps = SharedProps & {
+  title?: string;
 };
 
 const ListItem: FC<ListItemProps> = ({
   style,
   size: sizeProp,
   title,
+  centered,
+  titleStyle,
+  bodyStyle,
   titleSize,
+  variant,
   children,
 }) => {
-  const { direction, centered, size: sizeContext } = useContext(ListContext);
-  const size = sizeProp || sizeContext;
+  const {
+    direction,
+    centered: centeredContext,
+    size: sizeContext,
+    titleSize: titleSizeContext,
+    titleStyle: titleStyleContext,
+    bodyStyle: bodyStyleContext,
+    variant: variantContext,
+  } = useContext(ListContext);
+  const { variants } = useTheme();
+  const sharedStyle: CSSProperties = {
+    ...(variants ? { color: variants[variant ?? variantContext].color } : null),
+  };
 
   const Wrapper = direction === "vertical" ? Row : Column;
 
@@ -73,17 +106,30 @@ const ListItem: FC<ListItemProps> = ({
     <Wrapper style={{ verticalAlign: "top", ...style }}>
       {title ? (
         <Text
-          centered={centered}
-          size={titleSize || size}
-          style={{ fontWeight: 700, margin: 0, marginBottom: "4px" }}
+          centered={centered ?? centeredContext}
+          size={titleSize ?? titleSizeContext ?? sizeProp ?? sizeContext}
+          style={{
+            fontWeight: 700,
+            margin: 0,
+            marginBottom: "4px",
+            ...sharedStyle,
+            ...titleStyleContext,
+            ...titleStyle,
+          }}
         >
           {title}
         </Text>
       ) : null}
       <Text
-        centered={centered}
-        size={size}
-        style={{ margin: 0, marginBottom: "24px" }}
+        centered={centered ?? centeredContext}
+        size={sizeProp ?? sizeContext}
+        style={{
+          margin: 0,
+          marginBottom: "24px",
+          ...sharedStyle,
+          ...bodyStyleContext,
+          ...bodyStyle,
+        }}
       >
         {children}
       </Text>
