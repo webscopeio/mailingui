9768039991e71592a59aa84dfa8fcd10f09cd923
eric valcik <eric.valcik@gmail.com>
2023-06-22 10:38:54 +0200

    use regular object as const for themes

--- src/mailingui/themes/stitches.config.ts
+++ src/mailingui/themes/stitches.config.ts
@@ -3,34 +3,30 @@ import { createStitches } from "@stitches/react";
 /**
  * This instance of Stitches is for managing token themes
  */
-const {
-  config: { theme },
-} = createStitches({
-  theme: {
-    bg: {
-      default: "#f9fafb",
-      subtle: "#F1F5F9",
-      primary: "#2563EB",
-      secondary: "#fff",
-      success: "#f0fdf4",
-      warning: "#fffbeb",
-      danger: "#fef2f2",
-    },
-    text: {
-      default: "#4b5563",
-      subtle: "#2563EB",
-      primary: "#fff",
-      secondary: "#1E293B",
-      success: "#059669",
-      warning: "#b45309",
-      danger: "#b91c1c",
-    },
-    rounded: {
-      md: "4px",
-      full: "9999px",
-    },
+const theme = {
+  bg: {
+    default: "#f9fafb",
+    subtle: "#F1F5F9",
+    primary: "#2563EB",
+    secondary: "#fff",
+    success: "#f0fdf4",
+    warning: "#fffbeb",
+    danger: "#fef2f2",
   },
-} as const); // We define it as const so values can be read only
+  text: {
+    default: "#4b5563",
+    subtle: "#2563EB",
+    primary: "#fff",
+    secondary: "#1E293B",
+    success: "#059669",
+    warning: "#b45309",
+    danger: "#b91c1c",
+  },
+  rounded: {
+    md: "4px",
+    full: "9999px",
+  },
+} as const;
 
 /**
  * This instance of Stitches is for styling components and injecting styles into emails
