de8a483c8a4777032f2241d806ce8afc37cd8b24
Jan Shanel <shanel@webscope.io>
2023-06-26 12:20:38 +0200

    feat code el with copy button

--- src/docs/components/InstallationDocs/ProsePre.tsx
+++ src/docs/components/InstallationDocs/ProsePre.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import * as React from "react";
+import { CopyButton } from "@components/CopyButton";
 import { cn } from "@utils/cn";
 
-export const ProsePre = (props: React.HTMLAttributes<HTMLPreElement>) => (
-  <pre
-    {...props}
-    className={cn(
-      props.className,
-      "rounded-xl border border-solid border-dark-700 bg-dark-800 px-4 py-3"
-    )}
-  />
-);
+export const ProsePre = (props: React.HTMLAttributes<HTMLPreElement>) => {
+  const [code, setCode] = React.useState("");
+  return (
+    <div className="not-prose flex items-center justify-between rounded-xl border border-solid border-dark-700 bg-[#011627] px-5 py-3 text-base">
+      <pre
+        {...props}
+        ref={(node) => {
+          node?.textContent && setCode(node.textContent);
+        }}
+        className={cn(
+          props.className,
+          "not-prose border-0 my-0 bg-transparent"
+        )}
+      />
+      <div className="hidden self-start md:inline-flex">
+        <CopyButton code={code} />
+      </div>
+    </div>
+  );
+};
