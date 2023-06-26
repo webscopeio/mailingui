b65296bbda6154b415d8a4f8fc7f1b03f20002db
eric valcik <eric.valcik@gmail.com>
2023-06-26 12:59:21 +0200

    finish dividers

--- src/docs/examples/dividers/ThemedDividerWithText.tsx
+++ src/docs/examples/dividers/ThemedDividerWithText.tsx
@@ -1,42 +1,30 @@
 import * as React from "react";
 import { Body, Container, Head, Html, Preview } from "@react-email/components";
-import { Divider, Text, Theme, ThemeProvider } from "@mailingui/components";
-import { defaultTheme } from "@mailingui/themes";
+import { Divider, Text } from "@mailingui/components";
 
 const ThemedDividerWithText = () => {
-  const theme: Theme = {
-    ...defaultTheme,
-    variants: {
-      ...defaultTheme.variants,
-      secondary: {
-        backgroundColor: "#fff",
-        color: "#711696",
-        borderColor: "#b786ce",
-      },
-    },
-  };
   return (
     <Html>
       <Head />
       <Preview>Themed Divider With Text</Preview>
       <Body style={main}>
-        <ThemeProvider theme={theme}>
-          <Container style={container}>
-            <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </Text>
-            <Divider baseWidth={440}>Divider</Divider>
-            <Text>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum.
-            </Text>
-          </Container>
-        </ThemeProvider>
+        <Container style={container}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Text>
+          <Divider baseWidth={440} variant="brand">
+            Divider
+          </Divider>
+          <Text>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </Text>
+        </Container>
       </Body>
     </Html>
   );
