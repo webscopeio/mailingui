813f5e10e0c0f55645c4de65533f9f169d03d989
eric valcik <eric.valcik@gmail.com>
2023-06-26 13:43:02 +0200

    Refactor button examples, fix imports in button

--- src/docs/examples/buttons/CustomBorderRadiusButtons.tsx
+++ src/docs/examples/buttons/CustomBorderRadiusButtons.tsx
@@ -1,40 +1,37 @@
 import * as React from "react";
 import { Head, Body, Container, Html, Preview } from "@react-email/components";
-import { Button, ThemeProvider } from "@mailingui/components";
-import { defaultTheme } from "@mailingui/themes";
+import { Button } from "@mailingui/components";
 
 const CustomBorderRadiusButtons = () => (
   <Html>
     <Head />
     <Preview>Custom Border Radius Buttons</Preview>
     <Body style={main}>
-      <ThemeProvider theme={defaultTheme}>
-        <Container style={container}>
-          <Button href="#clicked!" size="xs" style={{ borderRadius: 10 }}>
-            Button
-          </Button>
-          <span style={{ padding: 10 }} />
-          <Button href="#clicked!" size="sm" style={{ borderRadius: 12 }}>
-            Button
-          </Button>
-          <span style={{ padding: 10 }} />
-          <Button href="#clicked!" size="md" style={{ borderRadius: 12 }}>
-            Button
-          </Button>
-          <span style={{ padding: 10 }} />
-          <Button href="#clicked!" size="lg" style={{ borderRadius: 14 }}>
-            Button
-          </Button>
-          <span style={{ padding: 10 }} />
-          <Button href="#clicked!" size="xl" style={{ borderRadius: 14 }}>
-            Button
-          </Button>
-          <span style={{ padding: 10 }} />
-          <Button href="#clicked!" size="2xl" style={{ borderRadius: 16 }}>
-            Button
-          </Button>
-        </Container>
-      </ThemeProvider>
+      <Container style={container}>
+        <Button href="#clicked!" size="xs" style={{ borderRadius: 10 }}>
+          Button
+        </Button>
+        <span style={{ padding: 10 }} />
+        <Button href="#clicked!" size="sm" style={{ borderRadius: 12 }}>
+          Button
+        </Button>
+        <span style={{ padding: 10 }} />
+        <Button href="#clicked!" size="md" style={{ borderRadius: 12 }}>
+          Button
+        </Button>
+        <span style={{ padding: 10 }} />
+        <Button href="#clicked!" size="lg" style={{ borderRadius: 14 }}>
+          Button
+        </Button>
+        <span style={{ padding: 10 }} />
+        <Button href="#clicked!" size="xl" style={{ borderRadius: 14 }}>
+          Button
+        </Button>
+        <span style={{ padding: 10 }} />
+        <Button href="#clicked!" size="2xl" style={{ borderRadius: 16 }}>
+          Button
+        </Button>
+      </Container>
     </Body>
   </Html>
 );
