3f47280a6d29e3cb708a694fbf059d812ce154e5
eric valcik <eric.valcik@gmail.com>
2023-07-03 15:37:38 +0200

    cleanup index.ts from installationdocs

--- src/docs/components/InstallationDocs/index.ts
+++ src/docs/components/InstallationDocs/index.ts
@@ -1,16 +1,8 @@
-import {
-  ComponentSourceTabs,
-  ComponentSourceTabsProps,
-} from "./ComponentSourceTabs";
-import { ComponentDemo } from "./ComponentDemo";
-import { ProsePre } from "./ProsePre";
-
-export { ProsePre, type ComponentSourceTabsProps };
+export { type ComponentSourceTabsProps } from "./ComponentSourceTabs";
+export { ProsePre } from "./ProsePre";
 export { DocArticle } from "./DocArticle";
-
 export { ComponentDemo } from "./ComponentDemo";
 export { ComponentSourceTabs } from "./ComponentSourceTabs";
-
 export {
   getComponentExampleProps,
   getComponentSource,
