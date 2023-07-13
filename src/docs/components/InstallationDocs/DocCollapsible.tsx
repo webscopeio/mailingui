202ea0092142501bebae86aaaf9127e09c150e17
Jan Shanel <shanel@webscope.io>
2023-07-13 13:29:02 +0200

    make doc border more subtle

--- src/docs/components/InstallationDocs/DocCollapsible.tsx
+++ src/docs/components/InstallationDocs/DocCollapsible.tsx
@@ -6,7 +6,8 @@ import {
 const defaultOptions: Omit<CollapsibleContentProps, "children"> = {
   expandLabel: "Show more",
   collapseLabel: "Hide section",
-  className: "space-y-8 md:space-y-12 border-l border-stone-700 pl-2 md:pl-4",
+  className:
+    "space-y-8 md:space-y-12 border-l md:border-l-2 border-neutral-800 pl-2 md:pl-4",
   collapsedSize: "max-h-[260px]",
 };
 
