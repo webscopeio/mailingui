12f8de3138e12b100871d4e2b5162d53576e01be
Hector Sosa <hello@hectorsosa.me>
2023-07-20 17:05:01 +0200

    Adding automatic installation instructions

--- src/docs/components/InstallationDocs/DocCollapsible.tsx
+++ src/docs/components/InstallationDocs/DocCollapsible.tsx
@@ -6,8 +6,7 @@ import {
 const defaultOptions: Omit<CollapsibleContentProps, "children"> = {
   expandLabel: "Show more",
   collapseLabel: "Hide section",
-  className:
-    "space-y-8 md:space-y-12 border-l md:border-l-2 border-neutral-800 pl-2 md:pl-4",
+  className: "space-y-8 md:space-y-12",
   collapsedSize: "max-h-[260px]",
 };
 
