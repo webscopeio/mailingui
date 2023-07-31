a5035d685d6a81a3efe1f4f51707a8503844e2e4
Hector Sosa <hello@hectorsosa.me>
2023-07-31 11:04:50 +0200

    Updating Documentation (#183)
    * Updating Documentation
    
    * Correct Link Typo
    
    * Updating Link Typo
    
    * Adding explicit instructions for install
    
    * Updating typo

--- src/docs/components/MdxComponents/DocArticle.tsx
+++ src/docs/components/MdxComponents/DocArticle.tsx
@@ -3,9 +3,6 @@ import { cn } from "@utils/cn";
 export const DocArticle = (props: React.HTMLAttributes<HTMLDivElement>) => (
   <article
     {...props}
-    className={cn(
-      props.className,
-      "mx-auto max-w-none space-y-8 md:space-y-12"
-    )}
+    className={cn(props.className, "mx-auto max-w-none space-y-9")}
   />
 );
