002375021989fe7f5c5adee7d54c0c2bba261cde
Jan Shanel <shanel@webscope.io>
2023-08-03 12:42:33 +0200

    shiki cache + no explanation

--- src/docs/lib/shiki.ts
+++ src/docs/lib/shiki.ts
@@ -11,19 +11,30 @@ const theme: Theme = "github-dark";
 const defaultlangs: Lang[] = ["html", "tsx"];
 const bg: React.CSSProperties["backgroundColor"] = "#011627";
 
+const highlighterCache = new Map<string, Promise<Highlighter>>();
+
 export async function getHighlighter({
   langs = defaultlangs,
 }: {
   langs?: Lang[];
-} = {}) {
+} = {}): Promise<Highlighter> {
   /** Preload NO languages in development */
   const isDevelopment = process.env.NODE_ENV === "development";
 
-  /* ✅ Create a highlighter instance with a theme */
-  return await getHighlighterFromShiki({
-    theme,
-    langs: isDevelopment ? [] : langs,
-  });
+  const key = [theme, ...langs].join(",");
+  if (!highlighterCache.has(key))
+    highlighterCache.set(
+      key,
+      /* ✅ Create a highlighter instance with a theme */
+      getHighlighterFromShiki({
+        theme,
+        langs: isDevelopment ? [] : langs,
+      })
+    );
+
+  const promise = highlighterCache.get(key);
+  if (!promise) throw new Error("Promise not found");
+  return await promise;
 }
 
 export async function highlight(
@@ -38,7 +49,8 @@ export async function highlight(
   const tokens = highlighter.codeToThemedTokens(
     code,
     isDevelopment ? "" : lang,
-    theme
+    theme,
+    { includeExplanation: false }
   );
   /* ⚠️ Optional: Custom rendering of code blocks */
   return renderToHtml(tokens, {
