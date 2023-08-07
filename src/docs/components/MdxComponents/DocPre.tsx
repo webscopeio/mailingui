eafbb9bcee12e61ff6a4da00824fd5b749625efe
eric valcik <eric.valcik@gmail.com>
2023-08-07 11:19:54 +0200

    Style the mdx codeblock better

--- src/docs/components/MdxComponents/DocPre.tsx
+++ src/docs/components/MdxComponents/DocPre.tsx
@@ -7,12 +7,12 @@ import { cn } from "@utils/cn";
 export const DocPre = ({ children }: { children: React.ReactNode }) => {
   const [code, setCode] = React.useState("");
   return (
-    <div className="flex items-center justify-between overflow-x-auto rounded-xl border border-solid border-dark-700 bg-[#011627] px-5 py-3 text-sm">
+    <div className="flex items-center justify-between overflow-x-auto rounded-md bg-[#011627] px-5 py-3 text-sm">
       <pre
         ref={(node) => {
           node?.textContent && setCode(node.textContent);
         }}
-        className={cn(" border-0 my-0 bg-transparent")}
+        className={cn("border-0 my-0 pl-7 bg-transparent")}
       >
         {children}
       </pre>
