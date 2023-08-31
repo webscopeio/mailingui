078ea8580a7e5346ffe9d4db8581dc74543c740f
Hector Sosa <hello@hectorsosa.me>
2023-08-31 11:29:11 +0200

    Updating Blob

--- src/components/landing/Homepage.tsx
+++ src/components/landing/Homepage.tsx
@@ -20,11 +20,11 @@ export const Homepage = () => {
     <>
       {/* GRADIENT BLOB */}
       <div
-        className="absolute inset-y-0 -z-10 transform-gpu overflow-hidden blur-3xl"
+        className="absolute -z-10 transform-gpu overflow-hidden blur-3xl"
         aria-hidden="true"
       >
         <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] scale-150 bg-gradient-to-tr from-[#80afff] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          className="relative aspect-video h-[40vh] w-[60vw] bg-gradient-to-tr from-indigo-400 to-violet-500 opacity-50 md:-translate-x-1/4 md:scale-150 md:opacity-30"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
