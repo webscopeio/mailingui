f61b6d08a8a52e4f17df71a9664637adc5d9128f
Hector Sosa <hello@hectorsosa.me>
2023-09-12 11:53:27 +0200

    Updating CLI

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -2,7 +2,4 @@ export const SETTINGS_FILE_NAME = "mailingui.json";
 
 export const DEPENDENCIES = ["react-email", "@react-email/components", "@mdx-js/react"];
 
-export const BASE_URL = "http://localhost:3000";
-
-// export const BASE_URL =
-//   process.env.COMPONENTS_BASE_URL ?? "https://mailingui.com";
+export const BASE_URL = process.env.COMPONENTS_BASE_URL ?? "https://mailingui.com";
