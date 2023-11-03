6f2043fc96f97e6f4dd5de1377e356a4ece2a933
Hector Sosa <hello@hectorsosa.me>
2023-11-03 10:42:12 +0100

    Need development

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Link from "next/link";
 import { email, safeParse, string } from "valibot";
-import { MailingUILogo } from "../MailingUILogo";
+import { ExternalLinkIcon } from "lucide-react";
+import { WebscopeLogo } from "@components/WebscopeLogo";
 import { CTA } from "@components/ui/CTA";
 
 /** ID of our form in ConvertKit. */
@@ -17,13 +18,27 @@ export const Footer = () => {
   return (
     <div className="bg-stone-900">
       <footer className="mx-auto flex max-w-[90rem] flex-col justify-between gap-y-8 px-6 pb-24 pt-12 md:flex-row">
-        <div className="w-fit">
-          <Link href="/" className="hover:opacity-75">
-            <MailingUILogo />
-          </Link>
-          <div className="mt-4">
-            &copy; {currentYear} MailingUI. All rights reserved.
+        <div className="w-fit space-y-3">
+          <div className="space-y-1.5">
+            <Link href="https://webscope.io" className="hover:opacity-75">
+              <WebscopeLogo />
+            </Link>
+            <p className="text-sm">
+              &copy; {currentYear} MailingUI by Webscope.io. All rights
+              reserved.
+            </p>
           </div>
+          <span className="relative inline-block before:absolute before:-inset-1 before:block before:h-2 before:translate-y-6 before:-skew-y-1 before:bg-indigo-700 before:opacity-50 before:transition-transform hover:before:translate-y-7 hover:before:opacity-40">
+            <span className="relative">
+              <Link
+                className="flex items-center gap-x-2 text-lg font-medium"
+                href="https://webscope.io"
+              >
+                I need development
+                <ExternalLinkIcon className="h-4 w-4" />
+              </Link>
+            </span>
+          </span>
         </div>
         <div className="space-y-2">
           <p className="text-sm text-gray-400">Join our Newsletter</p>
@@ -51,7 +66,11 @@ export const Footer = () => {
                   setNewsletterForm({ email: e.target.value, isValid: true });
                 }}
               />
-              <CTA compact className="sm:w-fit" disabled={!newsletterForm.isValid}>
+              <CTA
+                compact
+                className="sm:w-fit"
+                disabled={!newsletterForm.isValid}
+              >
                 Subscribe
               </CTA>
             </div>
